c29b5a633f04f5e1de24af200eabfdd227d2eca0
Robert Hieger <rhieger@gmail.com>
2020-10-24 16:20:30 -0400

    Create calculator.js with math functions for testing

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const mathOperations = {
+  sum: function(a, b) {
+    return a + b;
+  },
+  difference: function(a, b) {
+    return a - b;
+  },
+  product: function(a, b) {
+    return a * b;
+  },
+  quotient: function(a, b) {
+    return a / b;
+  },
+  modulus: function(a, b) {
+    return a % b;
+  }
+};
+
+module.exports = mathOperations;
